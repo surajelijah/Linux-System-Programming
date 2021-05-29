5908af978cac4a79c5fe55429327745a98cf4c9c
Suraj Elijah <suraj.elijah@gmail.com>
2021-05-29 23:37:31 +0530

    Initial commit

---
+++ Doubly_Linked_List_Library/dll.h
@@ -0,0 +1,15 @@
+
+typedef struct dll_node_{
+	void* data;
+	struct dll_node_* next;
+        struct dll_node_* prev;	
+}dll_node;
+
+typedef struct dll_list_{
+	dll_node *head;
+}dll_list;
+
+dll_list* create_list();
+
+void insert(dll_list **dll,void* data);
+void print_list(dll_list *dll);
